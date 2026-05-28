3e66f897b9f17152dd6a716240d9fbbacc990cea
yellowccake <negarjavid0@gmail.com>
2026-05-28 10:07:47 +0200

    ROF changed to 3.25

--- Rocket_calc_injector.py
+++ Rocket_calc_injector.py
@@ -8,10 +8,10 @@ F = 40_000                 # thrust [N]
 g0 = 9.81                  # gravity [m/s^2]
 pc = 80e5                  # chamber pressure [Pa]
 
-ROF = 3.5                  # oxidizer/fuel ratio
+ROF = 3.25                 # oxidizer/fuel ratio
 
-c_ideal = 3084.1           # ideal exhaust velocity from CEA [m/s]
-cstar_ideal = 1845.2       # ideal c* from CEA [m/s]
+c_ideal = 3100.3           # ideal exhaust velocity from CEA [m/s]
+cstar_ideal =1866       # ideal c* from CEA [m/s]
 
 eta_isp = 0.88             # impulse efficiency
 eta_cf = 0.96              # nozzle / CF efficiency
@@ -75,7 +75,6 @@ A_face = contraction_ratio * A_throat
 
 # Face plate diameter
 d_face = 2 * math.sqrt(A_face / math.pi)
-
 
 
 # OUTPUT
